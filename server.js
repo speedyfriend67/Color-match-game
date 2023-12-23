6525fc3dc56c756224ebd0c472973322e12a7931
Speedyfriend67 <82425907+speedyfriend67@users.noreply.github.com>
2023-12-24 00:44:55 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 const clients = new Map();
-const joinLeaveLog = [];
+let joinLeaveLog = loadJoinLeaveLog(); // Load the log from the file
 
 app.use(express.static('public'));
 
@@ -28,14 +29,21 @@ wss.on('connection', (ws) => {
   ws.on('close', () => {
     handleLeave(ws);
   });
+
+  // Send the initial join-leave log to the new client
+  ws.send(JSON.stringify({ type: 'joinLeaveLog', joinLeaveLog }));
 });
 
 function handleJoin(ws, username) {
   clients.set(ws, { username });
   broadcast({ type: 'join', username });
 
-  joinLeaveLog.push({ type: 'join', username, timestamp: new Date() });
+  const logEntry = { type: 'join', username, timestamp: new Date() };
+  joinLeaveLog.push(logEntry);
   broadcastJoinLeaveLog();
+
+  // Save the updated log to the file
+  saveJoinLeaveLog();
 }
 
 function handleMessage(ws, content) {
@@ -48,8 +56,12 @@ function handleLeave(ws) {
   clients.delete(ws);
   broadcast({ type: 'leave', username });
 
-  joinLeaveLog.push({ type: 'leave', username, timestamp: new Date() });
+  const logEntry = { type: 'leave', username, timestamp: new Date() };
+  joinLeaveLog.push(logEntry);
   broadcastJoinLeaveLog();
+
+  // Save the updated log to the file
+  saveJoinLeaveLog();
 }
 
 function broadcast(message) {
@@ -62,6 +74,20 @@ function broadcast(message) {
 
 function broadcastJoinLeaveLog() {
   broadcast({ type: 'joinLeaveLog', joinLeaveLog });
+}
+
+function saveJoinLeaveLog() {
+  fs.writeFileSync('joinLeaveLog.json', JSON.stringify(joinLeaveLog), 'utf-8');
+}
+
+function loadJoinLeaveLog() {
+  try {
+    const logData = fs.readFileSync('joinLeaveLog.json', 'utf-8');
+    return JSON.parse(logData);
+  } catch (error) {
+    console.error('Error loading join-leave log:', error.message);
+    return [];
+  }
 }
 
 const PORT = process.env.PORT || 3000;
